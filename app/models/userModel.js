143858c1b9502377982f8e58583cc57422005d2d
DurgaDevi <durgadevi398@gmail.com>
2019-02-04 20:20:51 +0530

    Update userModel.js

--- app/models/userModel.js
+++ app/models/userModel.js
@@ -3,10 +3,10 @@
 const mongoose = require('mongoose');
 var Schema = mongoose.Schema;   
 var UserSchema = new Schema({
-    name: String,
-    email: { type: String, lowercase: true },
-    contactNumber: String,
-    role: {
+    Name: String,
+    Email: { type: String, lowercase: true },
+    ContactNumber: String,
+    Role: {
         type: String,
         default: 'user'
     },
