dabb30295cfffb588b875ba625818ffc0b02cc83
DurgaDevi <durgadevi398@gmail.com>
2019-02-04 20:20:18 +0530

    Update itemsModel.js

--- app/models/itemsModel.js
+++ app/models/itemsModel.js
@@ -4,10 +4,10 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var ItemsSchema = new Schema({
-    image: String,
-    title: String,
-    description: String,
-    price: { type: Number },
+    ItemImage: String,
+    Title: String,
+    Description: String,
+    Price: { type: Number },
     isHotel: { type: Boolean, default: false },
     isSnack: { type: Boolean, default: false }
 },
